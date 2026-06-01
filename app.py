68b8de989f917c1f4b8685d209e332529decc18c
Andrest128 <143652939+Andrest128@users.noreply.github.com>
2026-06-01 15:15:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def cargar_datos():
     Promedia las 24 horas para obtener precio diario.
     """
     df_raw = pd.read_excel(
-        "Precio_Bolsa_TIE__kwh__2026.xlsx",
+        "Precio_Bolsa_TIE($kwh)_2026.xlsx",
         sheet_name="Precio_Bolsa_TIE(Valor_kwh)",
         header=2,
         parse_dates=["Fecha"],
